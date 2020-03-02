87e443673e3ce6975ea9ed1304ed141f734887e6
Keith Policano <kpolicano@gmail.com>
2020-03-02 11:57:40 -0600

    implementing controller

--- src/main/scala/com/linearframework/web/internal/OpenApiController.scala
+++ src/main/scala/com/linearframework/web/internal/OpenApiController.scala
@@ -1,15 +1,21 @@
 package com.linearframework.web.internal
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.linearframework.web._
 import com.linearframework.web.openapi.model.OpenApiDocument
 
 private[web] class OpenApiController(path: String, openApi: OpenApiDocument) extends Controller {
-  // todo: JSON
-//  private lazy val cache = openApi.json
-//
-//  GET(path) { (_, response) =>
-//    response.setContentType(JSON)
-//    cache
-//  }
+  private lazy val json = {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    mapper
+  }
 
+  private lazy val cache = json.writeValueAsString(openApi)
+
+  GET(path) { (_, response) =>
+    response.setContentType(JSON)
+    cache
+  }
 }
