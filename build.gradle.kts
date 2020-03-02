650c8228124349dd1eff76ad6c206a6427aeb627
Keith Policano <kpolicano@gmail.com>
2020-03-02 11:57:18 -0600

    adding json dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 
 dependencies {
   implementation("org.scala-lang:scala-library:2.13.1")
+  api("com.fasterxml.jackson.module:jackson-module-scala_2.13:2.10.2")
 
   // todo: replace the following block with repo-hosted linear-web
   api(fileTree("lib/linear-web-0.1.1-SNAPSHOT.jar"))
@@ -29,7 +30,6 @@ dependencies {
   testImplementation("junit:junit:4.13")
   testImplementation("org.scalatest:scalatest_2.13:3.1.1")
   testImplementation("org.scalatestplus:junit-4-12_2.13:3.1.1.0")
-  testImplementation("com.google.code.gson:gson:2.8.6")
 
   testRuntimeOnly("org.slf4j:slf4j-simple:1.7.25")
 }
@@ -42,8 +42,8 @@ tasks.named<Jar>("jar") {
 publishing {
   repositories {
     maven {
-      name = "LinearWeb"
-      url = uri("https://maven.pkg.github.com/linear-framework/linear-web")
+      name = "LinearWebOpenApi"
+      url = uri("https://maven.pkg.github.com/linear-framework/linear-web-openapi")
       credentials {
         username = System.getenv("GITHUB_USER")
         password = System.getenv("GITHUB_TOKEN")
@@ -52,7 +52,7 @@ publishing {
   }
   publications {
     create<MavenPublication>("PublishToGithub") {
-      artifactId = "web"
+      artifactId = "web-openapi"
       from(components["java"])
     }
   }
